66f1d50673ef47e0e401fa033ade9fb5b67a2b2f
hku2002 <hku2002@hanmail.net>
2024-03-03 19:17:22 +0900

    feat: output 추가

--- playground/practice01/local_file_random_string/main.tf
+++ playground/practice01/local_file_random_string/main.tf
@@ -1,12 +1,19 @@
 resource "local_file" "random_file" {
   filename              = var.filename
-  content               = random_string.random.id
+  content               = random_string.random.result
   directory_permission  = var.directory_permission
   file_permission       = var.file_permission
+  depends_on = [random_string.random]
 }
 
 resource "random_string" "random" {
   length  = 16
   special = false
   upper   = true
+}
+
+# terraform apply 후 terraform output random_code_output_result 을 하면 output 결과를 볼 수 있음
+output "random_code_output_result" {
+  value       = random_string.random.result
+  description = "랜덤 문제 결과값"
 }
