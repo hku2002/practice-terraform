e630ceee427aa244099469386c4b8f6ee2fff28d
hku2002 <hku2002@hanmail.net>
2024-03-06 21:46:51 +0900

    feat: iam 샘플 코드 추가

---
+++ playground/practice02/iam_sample/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.39.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+resource "aws_iam_user" "developer" {
+  name = "developer"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
